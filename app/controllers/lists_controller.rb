b5cdbc320041c80b298834ea6fbdf0fd10bee37b
Barbara Rojas <barbara.rojas.mercier@gmail.com>
2021-08-18 12:55:21 -0400

    avances

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,30 @@
 class ListsController < ApplicationController
+
     def index
-        @list = List.all
+      @lists = List.all
     end
     
     def show
-        @list = List.find(params[:movie])
+      @list = List.find(params[:id])
     end
     
-    #   def new
-    #     @restaurant = Restaurant.new
-    #   end
+    def new
+      @list = List.new
+    end
     
-    #   def create
-    #     @restaurant = Restaurant.new(Restaurant_params)
-    #     if Restaurant.save
-    #     redirect_to Restaurant_path(@restaurant)
-    #     else
-    #     render "new"
-    #   end
-    #   end
-    # end
+    def create
+      @list = List.new(List_params)
+    if List.save
+      redirect_to List_path(@list)
+    else
+      render "new"
+    end
+    end
+    end
     
-    #   private
+      private
     
-    #   def restaurant_paramas
-    #     params.require(:restaurant).permit(params[:name, :address, :phone_number, :category])
-    #   end
+      def list_paramas
+        params.require(:list).permit(params[:name, :created_at, :updated_at])
+      end
     
-end
