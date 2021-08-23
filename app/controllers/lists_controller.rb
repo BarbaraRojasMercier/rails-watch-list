ac3ec7c6aba6c030d2495c831fbb559d510686fc
Barbara Rojas <barbara.rojas.mercier@gmail.com>
2021-08-23 12:02:33 -0400

    Working exercise

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,39 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
-    def index
-      @lists = List.all
-    end
-    
-    def show
-      @list = List.find(params[:id])
-    end
-    
-    def new
-      @list = List.new
-    end
-    
-    def create
-      @list = List.new(List_params)
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @review = Review.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list)
     else
-      render "new"
+      render :new
     end
-    end
-    end
-    
-      private
-    
-      def list_params
-        params.require(:list).permit(params[:name])
-      end
-    
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
